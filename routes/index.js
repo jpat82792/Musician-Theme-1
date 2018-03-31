7e3cedcb7fe10734430e235b6277423e8db485f3
Jonathan <jpat82792@gmail.com>
2018-03-31 16:40:14 -0500

    add biography page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('news.html', { title: 'Express' });
 });
+router.get('/us', function(req, res, next){
+  res.render('about.html',{});
+});
+router.get('/shows', function(req, res, next){
+  res.render('shows.html', {});
+});
 
 module.exports = router;
